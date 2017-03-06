0a0d133ad566bd70c5d7ed332bd3fe0266a9391c
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-03-06 12:04:56 -0500

    Create managers and calculate manager bonuses

--- employee.rb
+++ employee.rb
@@ -1,15 +1,28 @@
 class Employee
   attr_reader :name, :title, :salary, :boss
 
-  def initialize(name, title, salary, boss)
+  def initialize(name, title, salary, boss = nil)
     @name = name
     @title = title
     @salary = salary
     @boss = boss
-    #boss.add self
+    boss.add_employee(self)
   end
 
   def bonus(mult)
     salary * mult
+  end
+end
+
+class Manager < Employee
+  attr_accessor :subordinates
+
+  def initialize(name, title, salary, boss)
+    super
+    @subordinates = []
+  end
+
+  def bonus(mult)
+    subordinates.map { |el| el.bonus(mult) }.reduce(:+)
   end
 end
