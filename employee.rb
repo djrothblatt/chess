1c83da07dc522fa2cf1951f66b7c980b412ad60f
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-03-06 11:54:33 -0500

    Create employee and calculate bonus

---
+++ employee.rb
@@ -0,0 +1,15 @@
+class Employee
+  attr_reader :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+    #boss.add self
+  end
+
+  def bonus(mult)
+    salary * mult
+  end
+end
