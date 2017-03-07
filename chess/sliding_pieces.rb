dea5d2cd08c48fa7f7736446007599903d11b920
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-03-07 11:41:35 -0500

    Add rook and queen; refactor SlidingPieces

--- chess/sliding_pieces.rb
+++ chess/sliding_pieces.rb
@@ -1,22 +1,20 @@
-require 'byebug'
+
 require_relative 'board'
 require_relative 'piece'
 
 module SlidingPiece
   def moves
     res = []
-    debugger
 
     move_dirs.each do |dir|
-      dx, dy = DIRECTIONS[dir]
+      dx, dy = directions[dir]
       current_pos = nudge(pos, dx, dy)
-      while board.in_bounds?(current_pos) && board[current_pos].is_a?(NullPiece)
+      while valid_move?(current_pos)
         res << current_pos
         current_pos = nudge(current_pos, dx, dy)
       end
     end
 
-    debugger
     res
   end
 
@@ -24,17 +22,6 @@ module SlidingPiece
     x, y = pos
     [x + dx, y + dy]
   end
-
-  DIRECTIONS = {
-    left: [0, -1],
-    right: [0, 1],
-    up: [-1, 0],
-    down: [1, 0],
-    up_left: [-1, -1],
-    up_right: [-1, 1],
-    down_left: [1, -1],
-    down_right: [1, 1]
-  }
 end
 
 class Bishop < Piece
@@ -45,13 +32,31 @@ class Bishop < Piece
   end
 
   def move_dirs
-    [:up_left, :up_right, :down_right, :down_left]
+    diagonals
   end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  board = Board.new
-  bishop = Bishop.new([1, 2], board, "green")
-  puts bishop.move_dirs
-  puts bishop.moves
+
+class Rook < Piece
+  include SlidingPiece
+
+  def initialize(pos, board, color)
+    super(pos, board, color, :R)
+  end
+
+  def move_dirs
+    straights
+  end
+end
+
+class Queen < Piece
+  include SlidingPiece
+
+  def initialize(pos, board, color)
+    super(pos, board, color, :Q)
+  end
+
+  def move_dirs
+    straights + diagonals
+  end
 end
