e3cfc6c97ba2dd59f5fe5584fe3fc2e34fd6656c
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-03-07 15:08:29 -0500

    Add king in_check?, refactor populate

--- chess/board.rb
+++ chess/board.rb
@@ -1,16 +1,23 @@
 require_relative 'piece'
 
 class Board
+  attr_reader :grid
+
+  BOARD_SIZE = 8
   def initialize
     @grid = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE, NullPiece.instance) }
+    @white_team = []
+    @black_team = []
     populate!
   end
 
   def populate!
-    STARTING_ROWS.each do |row|
-      (0...BOARD_SIZE).each { |col| self[[row, col]] = Piece.new([row,col], self, "blue", :P) }
-    end
-
+    place_kings
+    place_queens
+    place_bishops
+    place_knights
+    place_rooks
+    place_pawns
     nil
   end
 
@@ -47,10 +54,107 @@ class Board
     grid[x][y] = value
   end
 
+  def in_check?(color)
+    king, enemy_team = choose_opposing(color)
+    enemy_team.any? do |piece|
+      piece.moves.include?(king.pos)
+    end
+  end
+
+  def choose_opposing(color)
+    if color == :white
+      [white_king, black_team]
+    else
+      [black_king, white_team]
+    end
+  end
+
   private
-  attr_accessor :grid
-  BOARD_SIZE = 8
-  STARTING_ROWS = [0, 1, 6, 7]
+  attr_reader :white_king, :black_king
+  attr_accessor :black_team, :white_team
+
+  def place_kings
+    @black_king = King.new([0, 4], self, :black)
+    @white_king = King.new([7, 4], self, :white)
+    self[[0, 4]] = black_king
+    self[[7, 4]] = white_king
+
+    self.black_team += [black_king]
+    self.white_team += [white_king]
+  end
+
+  def place_queens
+    black_queen = Queen.new([0, 3], self, :white)
+    white_queen = Queen.new([7, 3], self, :white)
+
+    self[[0, 3]] = black_queen
+    self[[7, 3]] = white_queen
+
+    self.black_team += [black_queen]
+    self.white_team += [white_queen]
+  end
+
+  def place_bishops
+    black1 = Bishop.new([0, 2], self, :black)
+    black2 = Bishop.new([0, 5], self, :black)
+    white1 = Bishop.new([7, 2], self, :white)
+    white2 = Bishop.new([7, 5], self, :white)
+
+    self[[0, 2]] = black1
+    self[[0, 5]] = black2
+    self[[7, 2]] = white1
+    self[[7, 5]] = white2
+
+    self.black_team += [black1, black2]
+    self.white_team += [white1, white2]
+  end
+
+  def place_knights
+    black1 = Knight.new([0, 1], self, :black)
+    black2 = Knight.new([0, 6], self, :black)
+    white1 = Knight.new([7, 1], self, :white)
+    white2 = Knight.new([7, 6], self, :white)
+
+    self[[0, 1]] = black1
+    self[[0, 6]] = black2
+    self[[7, 1]] = white1
+    self[[7, 6]] = white2
+
+    self.black_team += [black1, black2]
+    self.white_team += [white1, white2]
+  end
+
+  def place_rooks
+    black1 = Rook.new([0, 0], self, :black)
+    black2 = Rook.new([0, 7], self, :black)
+    white1 = Rook.new([7, 0], self, :white)
+    white2 = Rook.new([7, 7], self, :white)
+
+    self[[0, 0]] = black1
+    self[[0, 7]] = black2
+    self[[7, 0]] = white1
+    self[[7, 7]] = white2
+
+    self.black_team += [black1, black2]
+    self.white_team += [white1, white2]
+  end
+
+  def place_pawns
+    place_row(1, :black)
+    place_row(6, :white)
+  end
+
+  def place_row(row, color)
+    @grid[row].each_index do |col|
+      pawn = Piece.new([row, col], self, color, :P)
+      self[[row, col]] = pawn
+      if color == :white
+        self.white_team += [pawn]
+      else
+        self.black_team += [pawn]
+      end
+    end
+  end
 end
 
 class NoPieceError < StandardError
